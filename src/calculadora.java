10c3230b5fd4828ff67375bca0635053f080a75a
Gonzalo Arca <Elgoncho1@gmail.com>
2023-02-10 14:45:04 +0100

    primer paso

---
+++ src/calculadora.java
@@ -0,0 +1,16 @@
+public class calculadora {
+
+
+    private static Float resultado = null;
+    static final int SUMA = 1;
+    static final int RESTA = 2;
+    static final int MULTIPLICACION = 3;
+    static final int DIVISION = 4;
+
+
+
+}
+
+
+
+

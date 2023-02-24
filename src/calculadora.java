c7f04c868638ab4b6b28e9f6aaae1a09d4fc23a3
Gonzalo Arca Iglesias <garcaiglesias@danielcastelao.org>
2023-02-24 09:44:07 +0100

    squash

--- src/calculadora.java
+++ src/calculadora.java
@@ -6,9 +6,10 @@ public class calculadora {
     static final int RESTA = 2;
     static final int MULTIPLICACION = 3;
     static final int DIVISION = 4;
+    static final int RAIZ = 5;
 
     /**
-     * @param device --> operación a realizar (SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN)
+     * @param device --> operación a realizar (SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN, RAIZ)
      * @param num1 --> primer numero de la operación
      * @param num2 --> segundo numero de la operación (divisor en caso de division)
      * @return resultado
@@ -44,6 +45,12 @@ public class calculadora {
                     System.out.println("Error");
                 }
                 break;
+            case RAIZ:
+                try{
+                    resultado = Math.pow(num1, 1.0 / num2); //num1 es el radicando y num2 el indice
+                }catch (Exception e){
+                    System.out.println("Error");
+                }
         }
         return resultado;
     }
